e1d6223106afb14863a749c3afeb08ca5558abb2
Lamzka <Raymond0besson@Gmail.com>
2025-05-07 09:58:32 +0200

    MovementProgress

--- RustGlide/Assets/Lamzka/Scripts/DashToDirection.cs
+++ RustGlide/Assets/Lamzka/Scripts/DashToDirection.cs
@@ -11,7 +11,13 @@ public class DashToDirection : MonoBehaviour
     public InputAction RightTrigger;
     public InputAction LeftTrigger;
 
+    public AudioClip ThrusterSound;
+    public AudioSource AudioSource;
+
     public float ThrustPower;
+
+    private bool HasRightBeenPressed;
+    private bool HasLeftBeenPressed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void OnEnable()
@@ -20,20 +26,52 @@ public class DashToDirection : MonoBehaviour
         LeftTrigger.Enable();
     }
 
+    private void Start()
+    {
+        AudioSource.clip = ThrusterSound;
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (RightTrigger.ReadValue<float>() > 0.1f)
         {
+            Debug.Log("RightHandTrigger");
+            AddForceToCubeRightDirection();
+            HasRightBeenPressed = true;
+        }
+        else if (HasRightBeenPressed && AudioSource.isPlaying)
+        {
+            StopNoise();
+            HasRightBeenPressed = false;
+        }
 
-            Debug.Log("it works");
-
-            AddForceToCubeDirection();
+        if (LeftTrigger.ReadValue<float>() > 0.1f)
+        {
+            Debug.Log("LeftHandTrigger");
+            AddForceToCubeLeftDirection();
+            HasLeftBeenPressed = true;
+        }
+        else if (HasLeftBeenPressed && AudioSource.isPlaying)
+        {
+            StopNoise();
+            HasLeftBeenPressed = false;
         }
     }
 
-    void AddForceToCubeDirection()
+    void AddForceToCubeRightDirection()
     {
+        AudioSource.PlayOneShot(ThrusterSound);
         PlayerRigidbody.AddForce(CubeRight.transform.forward * ThrustPower);
+    }
+    void AddForceToCubeLeftDirection()
+    {
+        AudioSource.PlayOneShot(ThrusterSound);
+        PlayerRigidbody.AddForce(CubeLeft.transform.forward * ThrustPower);
+    }
+
+    void StopNoise()
+    {
+        AudioSource.Stop();
     }
 }
