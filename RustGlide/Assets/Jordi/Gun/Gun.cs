88248c5f9f519d5b366e521cda05dfd388d1a628
Destroliedel22 <Jordiwalet@gmail.com>
2025-05-07 11:11:31 +0200

    gun completely done with input

--- RustGlide/Assets/Jordi/Gun/Gun.cs
+++ RustGlide/Assets/Jordi/Gun/Gun.cs
@@ -1,20 +1,31 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Gun : MonoBehaviour
 {
+    public InputAction Trigger;
+
     [SerializeField]private GameObject Bullethole;
     private int Dmg;
 
+    private void OnEnable()
+    {
+        Trigger.Enable();
+    }
+
     private void Update()
     {
-        RaycastHit hit;
-        if(Physics.Raycast(Bullethole.transform.position, Bullethole.transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+        if(Trigger.ReadValue<float>() > 0.1f)
         {
-            if(hit.collider.CompareTag("Enemy"))
+            RaycastHit hit;
+            if (Physics.Raycast(Bullethole.transform.position, Bullethole.transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
             {
-                //get hp and decrease it with Dmg
-                Debug.Log("hit");
+                if (hit.collider.CompareTag("Enemy"))
+                {
+                    //get hp and decrease it with Dmg
+                    Debug.Log("hit");
+                }
             }
         }
     }
