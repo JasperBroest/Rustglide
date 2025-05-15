0f27d163e914e8ff15ab7b7f49d1b114d6fae513
Lamzka <Raymond0besson@gmail.com>
2025-05-15 14:56:30 +0200

    merge

--- RustGlide/Assets/Lamzka/Scripts/HudManager.cs
+++ RustGlide/Assets/Lamzka/Scripts/HudManager.cs
@@ -6,9 +6,13 @@ public class HudManager : MonoBehaviour
 {
 
     public GameObject TextObject;
+
+
     public TextMeshPro Text;
 
     public float WaitBeforeBlink = 0.5f;
+
+    [SerializeField] private SOHudText[] hudText;
 
 
 
@@ -18,7 +22,7 @@ public class HudManager : MonoBehaviour
         StartCoroutine(TextStartSequence());
     }
 
-    public void StartEndSequence()
+    /*public void StartEndSequence()
     {
         StartCoroutine(EndSequence());
     }
@@ -26,7 +30,7 @@ public class HudManager : MonoBehaviour
     public void StartDeathSequence()
     {
         StartCoroutine(DeathSequence());
-    }
+    }*/
 
     public IEnumerator TextStartSequence()
     {
@@ -57,7 +61,7 @@ public class HudManager : MonoBehaviour
         TextObject.SetActive(false);
     }
 
-    public IEnumerator EndSequence()
+   /* public IEnumerator EndSequence()
     {
         Text.SetText("GOOD JOB");
         TextObject.SetActive(true);
@@ -124,5 +128,5 @@ public class HudManager : MonoBehaviour
         yield return new WaitForSeconds(WaitBeforeBlink);
         Text.SetText("KEEP MOVING");
 
-    }
+    }*/
 }
