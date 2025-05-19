96f1591855d780c01bd55085f2467e6e23252dff
Destroliedel22 <Jordiwalet@gmail.com>
2025-05-19 12:27:18 +0200

    dropchance added to roguelike

--- RustGlide/Assets/Jordi/RogueLikeSystem/RogueLikeManager.cs
+++ RustGlide/Assets/Jordi/RogueLikeSystem/RogueLikeManager.cs
@@ -1,30 +1,52 @@
 using System;
 using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
 
 public class RogueLikeManager : MonoBehaviour
 {
-    public List<GameObject> Upgrades;
+    public AllUpgrades[] Upgrades;
     public List<GameObject> ChosenUpgrades;
 
     private int RandValue;
 
+    [System.Serializable]
+    public struct AllUpgrades
+    {
+        public GameObject Upgrade;
+        [Range(0, 100)] public int DropChance;
+    }
+
     private void Start()
     {
-        if(Upgrades.Count > 0)
+        if(Upgrades.Length > 3)
         {
-            SelectThree();
+            GenerateThree();
         }
     }
 
-    private void SelectThree()
+    private void GenerateThree()
     {
         for(int i = 0; i < 3; i++)
         {
-            RandValue = UnityEngine.Random.Range(0, Upgrades.Count);
-            ChosenUpgrades.Add(Upgrades[RandValue]);
-            Upgrades.Remove(Upgrades[RandValue]);
-            Instantiate(Upgrades[RandValue]);
+            GameObject droppingItem = null;
+            int totalWeight = Upgrades.Sum(item => item.DropChance);
+            RandValue = UnityEngine.Random.Range(1, totalWeight + 1);
+            Debug.Log(RandValue);
+            int cumulative = 0;
+            foreach (var item in Upgrades)
+            {
+                cumulative += item.DropChance;
+                if (RandValue <= cumulative)
+                {
+                    droppingItem = item.Upgrade;
+                    break;
+                }
+            }
+            if(droppingItem != null)
+            {
+                ChosenUpgrades.Add(droppingItem);
+            }
         }
     }
 }
