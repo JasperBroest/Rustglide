9c345adc2cae034f4a487658929aac7967cadc62
Destroliedel22 <Jordiwalet@gmail.com>
2025-05-20 12:14:04 +0200

    added line for upgradin jumpmultiplier

--- RustGlide/Assets/Jordi/UpgradeGorilla.cs
+++ RustGlide/Assets/Jordi/UpgradeGorilla.cs
@@ -1,3 +1,4 @@
+using GorillaLocomotion;
 using Unity.XR.CoreUtils;
 using UnityEngine;
 
@@ -6,5 +7,6 @@ public class UpgradeGorilla : MonoBehaviour
     public void Upgrade()
     {
         XROrigin GorillaPlayer = FindFirstObjectByType<XROrigin>();
+        GorillaPlayer.GetComponentInChildren<Player>().jumpMultiplier++;
     }
 }
