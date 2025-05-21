1ee41486d280f416abbcb5322fe3cd8e197b1e4b
Destroliedel22 <Jordiwalet@gmail.com>
2025-05-21 12:12:24 +0200

    comments added to weapon script

--- RustGlide/Assets/Jordi/Weapon.cs
+++ RustGlide/Assets/Jordi/Weapon.cs
@@ -5,8 +5,6 @@ using UnityEngine.XR.Interaction.Toolkit.Utilities;
 
 public class Weapon : GunSubject
 {
-    public bool gunHeld = false;
-
     [SerializeField] protected GameObject Bullethole;
     [SerializeField] protected AudioClip GunShotAudio;
     [SerializeField] protected AudioClip GunHitAudio;
@@ -16,6 +14,7 @@ public class Weapon : GunSubject
 
     public InputAction Trigger;
     public ProTubeSettings gunHaptic;
+    public bool gunHeld = false;
 
     protected bool cooldownCoroutineRunning = false;
     protected int HandsHeld = 0;
@@ -29,12 +28,15 @@ public class Weapon : GunSubject
 
     protected void Shoot()
     {
+        //if you click the trigger, hold the gun and it isn't on cooldown plays particle, sound and haptic feedback
         if (Trigger.ReadValue<float>() > 0.1f && gunHeld && !onCooldown)
         {
             onCooldown = true;
             GunShotParticle.Play();
             gunShotSource.PlayOneShot(GunShotAudio);
             ForceTubeVRInterface.Shoot(gunHaptic);
+
+            //shoots a raycast out of the bullethole of the gun and spawns a particle and sound effect on the place you hit
             RaycastHit hit;
             if (Physics.Raycast(Bullethole.transform.position, Bullethole.transform.TransformDirection(Vector3.forward), out hit, Range))
             {
@@ -43,23 +45,29 @@ public class Weapon : GunSubject
                 SpawnedObject.transform.position = hit.point;
                 gunHitSource = SpawnedObject.GetComponent<AudioSource>();
                 gunHitparticle = SpawnedObject.GetComponentInChildren<ParticleSystem>();
+
+                //if the thing you hit is an enemy do damage to it and play enemy hit sound
                 if (hit.collider.CompareTag("Enemy"))
                 {
                     StateController stateController = hit.collider.GetComponent<StateController>();
                     stateController.ChangeState(stateController.HurtState);
                     gunHitSource.clip = EnemyHitAudio;
                 }
+                // if not then play normal hit sound
                 else
                 {
                     gunHitSource.clip = GunHitAudio;
                 }
                 gunHitparticle.Play();
                 gunHitSource.PlayOneShot(gunHitSource.clip);
+
+                //delete the spawned object with the audio and hit sound after a couple seconds
                 StartCoroutine(DeleteSource(SpawnedObject));
             }
         }
     }
 
+    //sets the cooldown of the gun so it doesnt shoot every frame
     public IEnumerator SetCooldown()
     {
         cooldownCoroutineRunning = true;
@@ -68,12 +76,14 @@ public class Weapon : GunSubject
         cooldownCoroutineRunning = false;
     }
 
+    //delete the spawned object with the audio and hit sound after a couple seconds
     public IEnumerator DeleteSource(GameObject gameObject)
     {
         yield return new WaitForSeconds(3f);
         Destroy(gameObject.gameObject);
     }
 
+    //checks if you grab if you hold it and also check for two handed objects
     public void OnGrab()
     {
         HandsHeld++;
@@ -85,6 +95,7 @@ public class Weapon : GunSubject
         }
     }
 
+    //checks if you release it if you hold it and also check for two handed objects
     public void OnRelease()
     {
         HandsHeld--;
